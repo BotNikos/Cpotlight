992156cf17a42c24356e9fb20838ae893fb2dfa7
BotNikos <boss.nikitos1212@mail.ru>
2024-06-23 11:09:55 +0300

    Added translation-mode and switch statement to choose prefix

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <curl/curl.h>
 
 const char *browser = "brave"; // Change browser, require recompilation
 
@@ -17,18 +18,57 @@ void calcualte (char *command) {
     execlp ("awk", "", calcCmd, NULL);
 }
 
+size_t translateOutput (char *data, size_t size, size_t nmemb, void *userData) {
+    static int callCount = 0;
+
+    if (callCount == 0) {
+        char *findStr = strstr (data, "Перевод");
+        findStr = strtok (findStr, ".");
+        puts (findStr);
+    }
+
+    callCount++;
+    return nmemb;
+}
+
+void translate (char *word) {
+    char link [64];
+    sprintf (link, "https://wooordhunt.ru/word/%s", word);
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL *handle = curl_easy_init ();
+    curl_easy_setopt(handle, CURLOPT_URL, link);
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, translateOutput);
+
+    CURLcode success = curl_easy_perform (handle);
+
+    if (success != CURLE_OK) 
+        printf ("%s", curl_easy_strerror(success));
+
+    curl_global_cleanup ();
+}
+
+int findElem (char *elem, char *array [], int size) {
+    for (int i = 0; i < size; i++) {
+        if (strcmp (elem, array[i]) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 void startProcess (char *prefix, char *command, char *userInputTrimmed) {
-    if (strcmp (prefix, "b") == 0)
-        execlp ("brave", "", command);
 
-    else if (strcmp (prefix, "bs") == 0) 
-        browserQuery(command);
+    char *prefixes [] = {"b", "bs", "c", "t"};
 
-    else if (strcmp (prefix, "c") == 0) 
-        calcualte(command);
-
-    else
-        execlp (userInputTrimmed, NULL);
+    switch (findElem (prefix, prefixes, sizeof (prefixes) / 8)) {
+        case 0: execlp ("brave", "", command); break;
+        case 1: browserQuery(command); break;
+        case 2: calcualte(command); break;
+        case 3: translate (command); break;
+        default: execlp (userInputTrimmed, NULL);
+    }
 }
 
 int main (void) {
@@ -43,10 +83,12 @@ int main (void) {
 
     int process = fork ();
 
-    if (process == 0) {
+    if (process == 0)
         startProcess (prefix, command, userInputTrimmed);
-    } else
-        getchar (); // Wait user input before exit
+    else
+        /* sleep (1); */ // Play with it for launcher auto-close
+        sleep (1); // Works perfect if start cpotlight from terminal manually
+        /* getchar (); // Wait user input before exit */
 
 
     return EXIT_SUCCESS;
