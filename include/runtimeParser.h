8e846c2b9562587311c2afedabffebbd393304ee
BotNikos <boss.nikitos1212@mail.ru>
2024-07-13 12:09:08 +0300

    translation and calculations complete

--- include/runtimeParser.h
+++ include/runtimeParser.h
@@ -2,8 +2,8 @@
 #define RUNTIME_PARSER_H
 
 char *parse (char *prefix, char *command, char *userInput);
-char *calcualte (char *command);
+void calcualte (char *command);
 size_t translateOutput (char *data, size_t size, size_t nmemb, void *userData) ;
-void translate (char *word);
+void translate (char *word, char *result);
 
 #endif
--- lib/runtimeParser.c
+++ lib/runtimeParser.c
@@ -7,54 +7,54 @@
 #include "../include/runtimeParser.h"
 #include "../include/helper.h"
 
-size_t translateOutput (char *data, size_t size, size_t nmemb, void *userData) {
-    static int callCount = 0;
+size_t translateOutput (char *data, size_t size, size_t nmemb, void *response) {
+    char *result = (char *) response;
 
-    if (callCount == 0) {
+    if (strcmp (result, "") == 0) {
         char *findStr = strstr (data, "Перевод");
         findStr = strtok (findStr, ".");
-        puts (findStr);
+        strcpy (result, findStr);
     }
 
-    callCount++;
     return nmemb;
 }
 
-void translate (char *word) {
+void translate (char *word, char *result) {
     char link [64];
+
     sprintf (link, "https://wooordhunt.ru/word/%s", word);
 
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *handle = curl_easy_init ();
     curl_easy_setopt(handle, CURLOPT_URL, link);
     curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, translateOutput);
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)result);
 
     CURLcode success = curl_easy_perform (handle);
 
     if (success != CURLE_OK)
-        printf ("%s", curl_easy_strerror(success));
+        strcpy (result, "Wrong input");
 
     curl_global_cleanup ();
 }
 
-char *calculate (char *command, char *result) {
+void calculate (char *command, char *result) {
     char calcCmd [32];
-    sprintf (calcCmd, "awk \"BEGIN {print %s}\"", command);
-
+    sprintf (calcCmd, "awk \"BEGIN {print %s}\" 2>&1", command);
+    
     FILE *calculations = popen (calcCmd, "r");
-    fgets(result, 31, calculations);
+    fgets(result, 255, calculations);
     pclose (calculations);
-    return result;
 }
 
 char *parse (char *prefix, char *command, char *userInputTrimmed) {
-    char *waitPrefixes [] = {"b", "bs"};
-    char *realTimePrefixes [] = {"c", "t"};
-    char *result = malloc (32);
+    char *prefixes [] = {"c", "t"};
+    char *result = malloc (256);
+    memset (result, '\0', 256);
 
-    switch (arrFind (prefix, realTimePrefixes, sizeof (realTimePrefixes) / 8)) {
+    switch (arrFind (prefix, prefixes, sizeof (prefixes) / 8)) {
         case 0: calculate (command, result); break;
-        /* case 1: translate (command); break; */
+        case 1: translate (command, result); break;
         default: strcpy (result, "Wrong command");
     }
 
