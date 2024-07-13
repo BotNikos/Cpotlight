71c7c894c856d722b4588e6642e3257373645afb
BotNikos <boss.nikitos1212@mail.ru>
2024-07-13 12:07:45 +0300

    deleted old defenitions

--- include/process.h
+++ include/process.h
@@ -5,10 +5,6 @@ static const char *browser = "brave";
 
 char *startProcess (char *prefix, char *command, char *userInputTrimmed);
 void browserQuery (char *command);
-char *calcualte (char *command);
-size_t translateOutput (char *data, size_t size, size_t nmemb, void *userData) ;
-void translate (char *word);
-int findElem (char *elem, char *array [], int size);
 
 #endif
  
